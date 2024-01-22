ac4c82450987c53b7e2d7b1877a46e17ff755a2b
ClementineAccount <ClementineShamaney@gmail.com>
2024-01-22 19:47:58 +0800

    SetDebugTitleActivity button to take to AR
    Sets the button to open the Intent.

--- app/src/main/java/com/csd3156/team7/DebugTitleActivity.kt
+++ app/src/main/java/com/csd3156/team7/DebugTitleActivity.kt
@@ -1,12 +1,29 @@
 package com.csd3156.team7
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.google.ar.core.examples.kotlin.helloar.HelloArActivity
 import com.google.ar.core.examples.kotlin.helloar.R
 
 class DebugTitleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_debug_title)
+
+        findViewById<Button>(R.id.buttonAR)
+            .setOnClickListener {
+                Log.d("BUTTONS", "User tapped the buttonAR")
+                openARScene()
+            }
+    }
+
+    fun openARScene()
+    {
+        Log.d("DEBUG TITLE", "openARScene() function called")
+        val intent = Intent(this, HelloArActivity::class.java)
+        startActivity(intent)
     }
 }
