d09882d49e25a7ac4d08101a4f6d565483b48821
ClementineAccount <ClementineShamaney@gmail.com>
2024-02-27 21:51:07 +0800

    Create DebugTestFarm Activity
    This activity will contain buttons that allow me to just input farms (on device) for testing reasons directly to test the Service.

---
+++ app/src/main/java/com/csd3156/team7/DebugTestFarm.kt
@@ -0,0 +1,12 @@
+package com.csd3156.team7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.ar.core.examples.kotlin.helloar.R
+
+class DebugTestFarm : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_debug_test_farm)
+    }
+}
