e13dfe8c0bb37782dc1d1f351e24de5285e78f00
ClementineAccount <ClementineShamaney@gmail.com>
2024-03-01 20:35:41 +0800

    FarmList Thing

--- app/src/main/java/com/csd3156/team7/FarmListRepository.kt
+++ app/src/main/java/com/csd3156/team7/FarmListRepository.kt
@@ -11,6 +11,7 @@ class FarmListRepository(private val farmDao: FarmDao) {
     // Room executes all queries on a separate thread.
     // Observed Flow will notify the observer when the data has changed.
     val alLFarms: Flow<List<FarmItem>> = farmDao.getAll()
+    val currentFarmShape : String = "Pyramid"
 
     // By default Room runs suspend queries off the main thread, therefore, we don't need to
     // implement anything else to ensure we're not doing long running database work
