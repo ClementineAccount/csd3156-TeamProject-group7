0d7a9ff329282895766624bed8bdcc812dbbea7e
ClementineAccount <ClementineShamaney@gmail.com>
2024-03-27 18:11:48 +0800

    Remove all the Mobile project stuff
    (We re-using the same repo so don't have to reinvite)

--- app/src/main/java/com/csd3156/team7/Weather/Condition.kt
+++
@@ -1,8 +0,0 @@
-package com.csd3156.team7.Weather
-
-import com.google.gson.annotations.SerializedName
-
-data class Condition(
-    @SerializedName("text")
-    val text: String
-)
--- app/src/main/java/com/google/ar/core/examples/kotlin/helloar/TapInterface.kt
+++
@@ -1,6 +0,0 @@
-package com.google.ar.core.examples.kotlin.helloar
-
-interface TapInterface {
-
-    fun onObjectTapped(farmId: Int)
-}
